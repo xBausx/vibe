74391348ab98c2868bcdba908160cce2c289756b
Rico Suan <ricos@n-compass.biz>
2025-07-04 10:49:15 +0800

    04 : background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,25 @@
-// "use client";
+"use client";
 
-// import { useTRPC } from "@/trpc/client";
-// import { useQuery } from "@tanstack/react-query";
-// import { caller } from '@/trpc/server';
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-import { getQueryClient, trpc } from "@/trpc/server";
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job started!");
+    }
+  }));
 
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-
-import { Suspense } from "react";
-
-import { Client } from "./client";
-
-//const Page = () => {
-const Page = async () => {
-  // const trpc = useTRPC();
-  // const { data } = useQuery(trpc.createAI.queryOptions({ text: "John" }));
-  // old : const { data } = useQuery(fetch("/api/create-ai", { body: JSON}))
-  //const data = await caller.createAI({ text : "Rico Server"});
-  // localhost:300/api/create-ai?body={text:"hello"}
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text : "Rico PREFETCH"}))
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      {/* {JSON.stringify(data)} */}
-      <Suspense>
-        <Client />
-      </Suspense>
-      
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Rico"})}>
+        Invoke Background Job
+      </Button>
     </div>
-    </HydrationBoundary>
   );
 }
 
